b15f1b8db4edaf3b32f881374d73bb8912bba24f
배주형 <wngud7971@gmail.com>
2026-09-14 22:27:19 +0900

    Revert "Add files via upload"
    This reverts commit 5a051cac1ea09d8da63a06187db91cf1cb1af307.

--- SafeHtml.js
+++
@@ -1,7 +0,0 @@
-import { sanitizeHtml } from "@/lib/sanitize";
-
-export default function SafeHtml({ html, className }) {
-  const clean = sanitizeHtml(html);
-  if (!clean || !clean.trim()) return null;
-  return <div className={className} dangerouslySetInnerHTML={{ __html: clean }} />;
-}
